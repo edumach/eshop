4dd8f77d44a6b13f540803ae98b05a0801d50ef1
edumach <59918768+edumach@users.noreply.github.com>
2025-11-02 17:58:58 +0100

    Create eshop database and sklad table

--- eshop.sql
+++ eshop.sql
@@ -1,9 +1,13 @@
+/* Pokud databáze eshop neexistuje, vytvoří ji */
 CREATE DATABASE IF NOT EXISTS eshop;
 
+/* Nastaví ji jako aktivní */
 USE eshop;
 
+/* Pokud tabulka sklad existuje, smaže ji */
 DROP TABLE IF NOT EXIST sklad;
 
+/* Vytvoří strukturu tabulky sklad */
 CREATE TABLE sklad (
   id int PRIMARY KEY AUTO_INCREMENT,
   kategorie VARCHAR(30),
@@ -12,10 +16,11 @@ CREATE TABLE sklad (
   pocet int unsigned NOT NULL DEFAULT 0
 );
 
-
+/* Vložení záznamů - varianta 1 */
 INSERT INTO sklad (kategorie, nazev, cena, pocet) VALUES ("Tričko", "Tričko modré", 200, 6);
 INSERT INTO sklad (kategorie, nazev, cena, pocet) VALUES ("Tričko", "Tričko zelené", 250, 10);
 
+/* Vložení záznamů - varianta 2 */
 INSERT INTO sklad (kategorie, nazev, cena, pocet) VALUES
 ("Deštník", "Deštník velký", 300, 5),
 ("Deštník", "Deštník malý", 150, 2),
